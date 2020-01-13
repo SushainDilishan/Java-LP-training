edff85b56e6fc5a310e4d8981603e1101c82f501
Sushain Dilishan <Sushaind96@gmail.com>
2020-01-13 21:21:04 +0530

    Files uploaded

---
+++ 002/a/Application.java
@@ -0,0 +1,9 @@
+package a;
+import c.Student;
+public class Application{
+    
+	public static void main(String [] args){
+        
+		new Student().print();
+    }
+}
---
+++ 002/c/Student.java
@@ -0,0 +1,9 @@
+package c;
+import b.Course;
+public class Student{
+    
+ 	public void print(){
+        
+		new Course().print();
+    }
+}
